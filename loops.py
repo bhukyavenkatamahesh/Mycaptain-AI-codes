5bf35e2750b3f8e99db5bf3a6493a7fbdbe3434d
Venkata Mahesh <bhukyavenkatamahesh@gmail.com>
2021-05-10 11:43:29 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,32 @@
+# Python program to print positive Numbers in a List
+
+# list of numbers
+list1 =  [12, -7, 5, 64, -14]
+num = 0
+
+# using while loop	
+while(num < len(list1)):
+	
+	# checking condition
+	if list1[num] >= 0:
+		print(list1[num], end = " ")
+        
+	
+	# increment num
+	num += 1
+
+# list of numbers
+print("\n")
+list2 = [12, 14, -95, 3]
+num = 0
+
+# using while loop	
+while(num < len(list2)):
+	
+	# checking condition
+	if list2[num] >= 0:
+		print(list2[num], end = " ")
+	
+	# increment num
+	num += 1
+	
